d88e0a1d036ec5b380b728bef0ef078075a15df3
bazingaterry <terry@32ph.com>
2016-11-09 18:34:10 +0800

    draft

---
+++ Lab-7/app/src/main/java/pub/terry/lab_7/FileActivity.java
@@ -0,0 +1,13 @@
+package pub.terry.lab_7;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_file);
+    }
+}
--- Lab-7/app/src/main/java/pub/terry/lab_7/MainActivity.java
+++ Lab-7/app/src/main/java/pub/terry/lab_7/MainActivity.java
@@ -2,6 +2,7 @@ package pub.terry.lab_7;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    void makeToast(String text) {
+        Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
     }
 }
