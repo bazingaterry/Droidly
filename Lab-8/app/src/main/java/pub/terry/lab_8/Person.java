add18d81c411716c965d18704540e6a4480e1722
bazingaterry <terry@32ph.com>
2016-11-17 13:13:20 +0800

    Class: Person

---
+++ Lab-8/app/src/main/java/pub/terry/lab_8/Person.java
@@ -0,0 +1,39 @@
+package pub.terry.lab_8;
+
+/**
+ * Created by terrychan on 17/11/2016.
+ */
+
+public class Person {
+    protected String name, birthday, gift;
+
+    public Person(String name, String birthday, String gift) {
+        this.name = name;
+        this.birthday = birthday;
+        this.gift = gift;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public String getGift() {
+        return gift;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    public void setGift(String gift) {
+        this.gift = gift;
+    }
+}
